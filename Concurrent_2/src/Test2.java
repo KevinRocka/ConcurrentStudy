452d9608f4e1cdc8ca7a2d4bcad4269cb9bb1162
任嘉为 <kevin.rocka@gmail.com>
2018-10-23 10:42:58 +0800

    init commit

---
+++ Concurrent_2/src/Test2.java
@@ -0,0 +1,12 @@
+/**
+ * @author: Rocka
+ * @version: 1.0
+ * @description: TODO
+ * @time:2018/10/23
+ */
+public class Test2 {
+
+    public static void main(String[] args){
+        System.out.println("Hello Word   222");
+    }
+}
